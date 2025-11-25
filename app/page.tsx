032b28eb9bc76f65f99a40faa3fd464c8cf544df
ayanhasnain03 <ayanhasnain2572006@gmail.com>
2025-11-26 02:53:10 +0530

    05:ai-jobs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 const page =  () => {
+  const [value, setValue] = useState("")
   const trpc = useTRPC()
   const invokeMutation = useMutation(trpc.invoke.mutationOptions({
     onSuccess:()=>{
@@ -14,10 +17,11 @@ const page =  () => {
       console.log(data)
     }
   }))
-  return <div className="max-w-7xl min-h-screen flex items-center justify-center">
+  return <div className="max-w-7xl min-h-screen flex items-center justify-center flex-col gap-2.5">
+    <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
     <Button className="min-w-md" onClick={()=>{
       invokeMutation.mutate({
-        text:"Invoked Success from my side"
+        value,
       })
     }}>
       Invoke
--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,12 +1,18 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
-
 export const helloWorld = inngest.createFunction(
   { id: "wow-ui-hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    console.log("Step started:", event.data.email);
-    await step.sleep("wait-a-moment", "1s");
-    console.log("Step finished:", event.data.email);
-    return { message: `Hello ${event.data.email}` };
+  async ({ event }) => {
+    const summerizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer. you summrize in 2 words.",
+      model: openai({ model: "gpt-3.5-turbo" })
+    })
+    const { output } = await summerizer.run(
+      `Summarize the following text: ${event.data.value}`
+    )
+    console.log(output)
+    return { output }
   }
 );
